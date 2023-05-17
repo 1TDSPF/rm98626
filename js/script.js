59e5068a0b8c8299abf67b774772b038233f82da
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-05-17 11:06:03 -0300

    calculadora finalizada

--- js/script.js
+++ js/script.js
@@ -1,124 +1,142 @@
-// //DIFERENÇA ENTRE LET/VAR/CONST
+// // DIFERENÇA ENTRE LET/VAR/CONST
 // // var nome = "Kayque";
 // // if(nome == "Kayque"){
 // //     let nome = "Denise";
 // // }
 // // console.log(nome);
 
-//  //Recuperando um elemento button que está no index.html
-//  const btn  = document.getElementById("meu-btn");
+// //  Recuperando um elemento button que está no index.html
+// //  const btn  = document.getElementById("meu-btn");
  
-//  //Atrelando um evento de click ao button que foi recuperado.
+// //  //Atrelando um evento de click ao button que foi recuperado.
+// //  btn.addEventListener("click" , function(){
 
-//  btn.addEventListener("click" , function(){
-    
-//     //função matematica - math
-//     //ranaon = gera numeros random entre 0 e 1 Ex: 0.3278687
-//     //floor = arredonda o número para baixo
-//     //ceil = arredonda o número para cima.
-//     //round = arredonda o número aleatoriamente
-//     let r,g,b;
-//     r = Math.round(Math.random()*255);
-//     g = Math.round(Math.random()*255);
-//     b = Math.round(Math.random()*255);
-    
-//     //adicionando um atributo ao elemento button
-//     //this.setAttribute("nome di atributo" , "valor do atributo")
-//     this.setAttribute("style" , `background-color: rgb(${r},${g},${b});`);
-//  } );
+// //     //Função Matemática - Math
+// //     //randon = gera números aleatórios enre 0 e 1. Ex: 0.232323;
+// //     //floor  = arredonda o número para baixo.
+// //     //ceil   = arredonda o número para cima.
+// //     //round  = arredonda o número aleatóriamente.
+// //     let r,g,b;
 
-// //desafio
-// //recupere o elemento tit-sec e atrele a ele um evento de mouse a sua escolha
-// //altere a cor de fundo e a cor do texto quando o evento ocorrer
+// //     r = Math.round(Math.random() *255);
+// //     g = Math.round(Math.random() *255);
+// //     b = Math.round(Math.random() *255);
 
-// const tit = document.getElementById("meu-tit");
+// //     //Adicionando um atributo ao elemeno button
+// //     // this.setAttribute("style", "background-color:rgb("+r+","+g+","+b+");");
+// //     this.setAttribute("style", `background-color:rgb(${r},${g},${b});`);
 
-// tit.addEventListener("mouseover", function(){
-
-//     let red,green,blue,red1,green1,blue1;
-//     red = Math.round(Math.random()*255);
-//     green = Math.round(Math.random()*255);
-//     blue = Math.round(Math.random()*255);
-//     red1 = Math.round(Math.random()*255);
-//     green1 = Math.round(Math.random()*255);
-//     blue1 = Math.round(Math.random()*255);
-
-//     this.setAttribute("style", `color: rgb(${red},${green},${blue});background-color: rgb(${red1},${green1},${blue1});`);
-    
+// //  } );
 
 
-// });
+// // //DESAFIO
+// // //Recupere o elemento tit-sec e atrele a ele um evento de mmouse a sua escolha.
+// // // Altere a cor de fundo e a cor do texto quando o evento ocorer.
+// // // const h2Element = document.getElementById("meu-tit");
+// // // // console.log(h2Element);
 
-// let frutas = ["maçã", "laranja", "banana", "uva", "caqui"]
-// console.log(frutas)
-// console.log("Tamanho do array: " + frutas.length)
-// console.log("Pegando um item do array: " + frutas[0]);
-// console.log("Pegando um item do array: " + frutas[1]);
-// console.log("Pegando um item do array: " + frutas[2]);
-// console.log("Pegando um item do array: " + frutas[3]);
-// console.log("Pegando um item do array: " + frutas[4]);
-// console.log("Pegando um item do array: " + frutas[5]);
-// //para verificar o tamanho do array, utilizamos a propriedade lenght
+// // // h2Element.addEventListener("click", function(){
+// // //     this.setAttribute("style", `background-color:#ff0000;color:#fff;`);
+// // // });
 
-// //iterando o array com estrutura de repetição : foreach
+// // const h2Element = document.querySelector("h2");
+// // // console.log(h2Element);
 
-// //add item no fim do array com push
-// frutas.push("melão");
+// // h2Element.addEventListener("click", function(){
+// //     this.setAttribute("style", `background-color:#ff0000;color:#fff;`);
+// // });
+
+
+// //Declarando um array
+// let frutas = ["maçã","laranja","banana","uva","kiwi","caqui"];
+// //Imprimindo o array
 // console.log(frutas);
+// //Para verificar o tamanho do array utilizamos a propriedade length
+// console.log("Tamanho do Array " + frutas.length);
+// //Imprimindo um item especifico do array.
+// console.log("Pegando um dos itens do Array : " + frutas[0]);
+// console.log("Pegando um dos itens do Array : " + frutas[1]);
+// console.log("Pegando um dos itens do Array : " + frutas[2]);
+// console.log("Pegando um dos itens do Array : " + frutas[3]);
+// console.log("Pegando um dos itens do Array : " + frutas[4]);
+// console.log("Pegando um dos itens do Array : " + frutas[5]);
 
-// //add item no começo do array com unshift
+// //Iterando o array com estruturas de repetição:
+// //forEach
+// frutas.forEach( (fruta)=>{
+//     console.log("Pegando um dos itens do Array com forEach: " + fruta);
+// } );
+
+// //Adicionado um item ao final do array com o método push(item);
+// frutas.push("melão");
+// console.table(frutas);
+
+// //Adicionado um item no início do array com o método unshift(item);
 // frutas.unshift("melancia");
 // console.table(frutas);
 
-// //removendo item do fim com pop
+// //Removendo um item do final do array com o método pop();
 // frutas.pop();
 // console.table(frutas);
 
-// //removendo item do começo com shift
+// //Removendo um item do inicio do array com o método pop();
 // frutas.shift();
 // console.table(frutas);
 
-// //localizando um determinado item dentro do array com indexOf(item);
+// //Localizando um determinando item dentro do array com o método indexOf(item);
+// // let indice = frutas.indexOf("banana");
+// // console.log("INDICE ONDE FOI LOCALIZADO O ITEM : " + indice);
+// // console.log("ITEM QUE FOI LOCALIZADO NO ARRAY : " + frutas[indice]);
+
+// //Utilizando os métodos, slice(indiceItem, quantidadeRemoções) para remover um item do array passando sua localizaçao através do indice.
+// //Localizando um determinando item dentro do array com o método indexOf(item);
 // let indice = frutas.indexOf("banana");
-// console.log("Item que estava no local antes da remoção: " + frutas[indice]);
-// frutas.splice(indice, 2)
-// console.log("item que foi localizado no array: " + frutas[indice]);
-// console.table(frutas)
+// console.log("ITEM QUE ESTAVA NO LOCAL ANTES DA REMOÇÃO : " + frutas[indice]);
+// frutas.splice(indice,2);
+// console.table(frutas);
+// console.log("ITEM QUE FICOU NO LOCAL APÓS A REMOÇÃO : " + frutas[indice]);
 
 // let nr1 = [1,2,3,4,5];
 // let nr2 = [6,7,8,9,10];
 // console.log(nr1);
 // console.log(nr2);
-
-// //concatenando arrays
-// let nr3 = [...nr1, ...nr2];
-// console.log("novo array: " + nr3);
-
-// //imprimindo o array concatenando com o operador spread( ... )
-// //imprimindo com foreach
-
+// //CONCATENANDO ARRAYS
+// let nr3 = [nr1,nr2];
+// console.log("NOVO ARRAY : " + nr3);
+// //Imprimindo o Array concatenado com forEach
 // nr3.forEach((nr)=>{
-//     console.log("item do novo array: " + nr)
+//     nr.forEach((n)=>{
+//         console.log("ITEM DO NOVO ARRAY : " + n);
+//     })
+// })
+// let nr1 = [1,2,3,4,5];
+// let nr2 = [6,7,8,9,10];
+// console.log(nr1);
+// console.log(nr2);
+// //CONCATENANDO ARRAYS COM O OPERADOR SPREAD( ... )
+// let nr3 = [...nr1,...nr2];
+// // //Imprimindo o Array concatenado com forEach
+// nr3.forEach((nr)=>{
+//     console.log("ITEM DO NOVO ARRAY : " + nr);    
 // });
 
 //convertendo HTMLCollection em Arrays
 // const imgElements = document.getElementsByTagName("img");
-
+// console.log(imgElements);
 // const imgElementsArray = [...imgElements];
-// console.log(imgElementsArray)
 // imgElementsArray.forEach((img)=>{
-//     img.setAttribute("width", "5%")
+//     img.setAttribute("width","3%");
 // });
 
-//recupere uma collections de elementos do tipo( a )
-//identifique aqueles que são pertencentes somente ao cabeçalho e add o atributo style com o a propriedade background-color: #ff0000
+//Recupere uma colections de elementos do tipo ( a ).
+// Identifique aqueles que são pertencentes somente ao cabeçalho e adicione o atributo style com a propriedade background-color:#ff0000;
+//Tire um print do código e da página alterada envie no chat!
 
 const aElements = [...document.getElementsByTagName("a")];
-console.log(aElements);
 
 aElements.forEach((a)=>{
     let textoDoA = a.textContent;
     if(textoDoA == "Home" || textoDoA == "Info" || textoDoA == "Item-3"){
-        a.setAttribute("style", `background-color: #ff0000`)
+        a.setAttribute("style","background-color:#ff0000;")
     }
 });
