e432d40973189907e8bc496e7c04fb6909a6bc98
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-05-21 11:17:17 -0300

    página de login finalizada

--- js/login.js
+++ js/login.js
@@ -1,44 +1,46 @@
-//CRIAR UMA LISTA DE OBJETOS
-let listaDeUsuarios = [
-    {
-        nomeCompleto : "Jaú da Selva",
-        usuarioEmail : "jau@email.com",
-        usuarioSenha : "123456"
-    },
-    {
-        nomeCompleto : "Paulo das Couves",
-        usuarioEmail : "paulo@email.com",
-        usuarioSenha : "12356"
-    },
-    {
-        nomeCompleto : "José da Silva",
-        usuarioEmail : "jose@email.com",
-        usuarioSenha : "12356"
-    },
-    {
-        nomeCompleto : "Maria do Socorro",
-        usuarioEmail : "maria@email.com",
-        usuarioSenha : "12356"
-    },
-    {
-        nomeCompleto : "André da Silva",
-        usuarioEmail : "andre@email.com",
-        usuarioSenha : "12356"
-    }
-];
+// //CRIAR UMA LISTA DE OBJETOS
+// let listaDeUsuarios = [
+//     {
+//         nomeCompleto : "Jaú da Selva",
+//         usuarioEmail : "jau@email.com",
+//         usuarioSenha : "123456"
+//     },
+//     {
+//         nomeCompleto : "Paulo das Couves",
+//         usuarioEmail : "paulo@email.com",
+//         usuarioSenha : "123456"
+//     },
+//     {
+//         nomeCompleto : "José da Silva",
+//         usuarioEmail : "jose@email.com",
+//         usuarioSenha : "123456"
+//     },
+//     {
+//         nomeCompleto : "Maria do Socorro",
+//         usuarioEmail : "maria@email.com",
+//         usuarioSenha : "123456"
+//     },
+//     {
+//         nomeCompleto : "André da Silva",
+//         usuarioEmail : "andre@email.com",
+//         usuarioSenha : "123456"
+//     }
+// ];
 
-//ADD A LISTA DE OBJETOS AO LOCAL - STORAGE
-localStorage.setItem("listaUser", JSON.stringify(listaDeUsuarios))
+// //ADICIONANDO A LISTA DE OBJETOS AO LOCAL-STORAGE
+// localStorage.setItem("listaUser", JSON.stringify(listaDeUsuarios));
+
 addEventListener("click", (evt)=>{
     
     const inputEmail = document.querySelector("#idEmail");
     const inputPass = document.querySelector("#idPass");
-
     
     if(evt.target.id == "btnSubmit"){
 
         try {
             
+            let listaDeUsuarios = JSON.parse(localStorage.getItem("listaUser"));
+
             listaDeUsuarios.forEach((usuario)=>{
      
                 if(inputEmail.value == usuario.usuarioEmail && inputPass.value == usuario.usuarioSenha){
@@ -60,12 +62,16 @@ addEventListener("click", (evt)=>{
             }
             
         }
-    }else if(evt.target.className="fa fa-eye" || "fa fa-eye-slash"){
+    //Ao mostrar a senha troque o ícone também pelo ícone de olho com um traço.
+
+    }else if(evt.target.className == "fa fa-eye" || evt.target.className == "fa fa-eye-slash"){
+
         if(inputPass.getAttribute("type") == "password"){
-            inputPass.setAttribute("type", "text");
-            evt.target.setAttribute("class" , "fa fa-eye-slash")
+            inputPass.setAttribute("type","text");
+            evt.target.setAttribute("class","fa fa-eye-slash");
         }else{
-            inputPass.setAttribute("type", "password")
+            inputPass.setAttribute("type","password");
+            evt.target.setAttribute("class","fa fa-eye");
         }
 
     }
